247fa4a2314cf570ccc732f2e3a7093871ecfffe
Baskaran Govindaras <saibaskar57@hotmail.com>
2020-04-05 16:35:28 +0800

    Added example key.

--- src/Tests/Service.Integration.Tests/Configuration.cs
+++ src/Tests/Service.Integration.Tests/Configuration.cs
@@ -3,7 +3,7 @@
     public static class Configuration
     {
         public static string EventServiceEndpoint = "http://localhost/event/api/callback";
-        public static string EventServiceKey = "";
+        public static string EventServiceKey = "example";
         public static string SubscriptionServiceEndpoint = "http://localhost/event/api/subscription";
     }
 }
